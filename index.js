197fa1b0a1c92b38f6792189bee4eeda816d49cb
Nathan Chung <chung.nathanh@gmail.com>
2020-05-27 23:09:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function deleteFromObjectByKey(object, key) {
   return newObj
 }
 
-function destructivelyDeleteFromObjectByKey(object, key) {
-  delete object[key]
+function destructivelyDeleteFromObjectByKey(pokemon, key) {
+  delete pokemon[key]
   return object
 }
